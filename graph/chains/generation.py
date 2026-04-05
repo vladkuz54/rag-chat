1fe86751e79ae904e8abdd2f6f4962d030c7aaec
vladkuz54 <vkuz1540@gmail.com>
2026-04-05 20:41:29 +0300

    Update generation.py to specify model in ChatOpenAI and correct prompt source

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -2,8 +2,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langsmith import Client
 
-llm = ChatOpenAI(temperature=0)
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
 client = Client()
-prompt = client.pull_prompt("rlm/rag-prompt")
+prompt = client.pull_prompt("daekeun-ml/rag-baseline")
 
 generation_chain = prompt | llm | StrOutputParser()
