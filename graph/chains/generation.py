5bf21868c4ed5b97772405f92b38374e7014dd2a
vladkuz54 <vkuz1540@gmail.com>
2026-04-05 12:08:53 +0300

    Set temperature parameter for ChatOpenAI initialization

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -2,7 +2,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langsmith import Client
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(temperature=0)
 client = Client()
 prompt = client.pull_prompt("rlm/rag-prompt")
 
