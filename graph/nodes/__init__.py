6727f3645fcc677678305c55530a0df87f8aa33e
vladkuz54 <vkuz1540@gmail.com>
2026-04-05 20:49:29 +0300

    Add generate_generic_response to __all__ in __init__.py

--- graph/nodes/__init__.py
+++ graph/nodes/__init__.py
@@ -1,7 +1,13 @@
 from graph.nodes.generate import generate
+from graph.nodes.generic_response import generate_generic_response
 from graph.nodes.grade_documents import grade_documents
 from graph.nodes.retrieve import retrieve
-from graph.nodes.web_search import web_search
 from graph.nodes.transform import transform
 
-__all__ = ["generate", "grade_documents", "retrieve", "web_search", "transform"]
+__all__ = [
+    "generate",
+    "grade_documents",
+    "retrieve",
+    "transform",
+    "generate_generic_response",
+]
