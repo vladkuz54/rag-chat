76977a87786cfdd083c34852065b812cdd301c22
vladkuz54 <vkuz1540@gmail.com>
2026-04-05 20:41:19 +0300

    Add generic_response.py for handling response generation based on retrieved context

---
+++ graph/chains/generic_response.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+llm = ChatOpenAI(temperature=0)
+
+system = """You a response writer that says that you do not know the anweser to the question based on a retrieved context. \n
+        Please provide quality answer with a explanation. \n
+        VITAL: You MUST return answer in the language of the inital question"""
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        (
+            "human",
+            "Here is a question: \n\n {question} \n\n Retrived context: {documents}",
+        ),
+    ]
+)
+
+generic_response_chain = prompt | llm | StrOutputParser()
