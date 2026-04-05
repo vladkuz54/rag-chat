a88b2c98d21a83cb4d89bc8171fa0d703055c442
vladkuz54 <vkuz1540@gmail.com>
2026-04-05 23:27:54 +0300

    Update import statement to use get_retriever for document retrieval

--- graph/nodes/retrieve.py
+++ graph/nodes/retrieve.py
@@ -1,14 +1,14 @@
 from typing import Any, Dict
 
 from graph.state import GraphState
-from ingestion import retriever
+from ingestion import get_retriever
 
 
 def retrieve(state: GraphState) -> Dict[str, Any]:
     print("---RETRIEVE---")
     question = state["question"]
 
-    documents = retriever.invoke(question)
+    documents = get_retriever().invoke(question)
     return {
         "documents": documents,
         "question": question,
