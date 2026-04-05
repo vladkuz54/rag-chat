a9d03dca52bcbedd8bb594d896d71e08e6ecc4d3
vladkuz54 <vkuz1540@gmail.com>
2026-04-05 12:09:05 +0300

    Refactor grade_documents to update transform logic and improve relevance checking

--- graph/nodes/grade_documents.py
+++ graph/nodes/grade_documents.py
@@ -7,22 +7,23 @@ from graph.state import GraphState
 def grade_documents(state: GraphState) -> Dict[str, Any]:
     """
     Determines whether the retrieved documents are relevant to the question
-    If any document is not relevant, we will set a flag to run web search
+    If documents are not relevant, we will set a flag to run transform of a question
 
     Args:
         state (dict): The current graph state
 
     Returns:
-        state (dict): Filtered out irrelevant documents and updated web_search state
+        state (dict): Filtered out irrelevant documents and updated transform state
     """
     print("---CHECK THE RELEVANCE TO QUESTION---")
 
     question = state["question"]
     documents = state["documents"]
+    transform_count = state.get("transform_count", 0)
 
     filtered_docs = []
 
-    web_search = False
+    transform = False
 
     for d in documents:
         score = retrieval_grader_chain.invoke(
@@ -36,7 +37,14 @@ def grade_documents(state: GraphState) -> Dict[str, Any]:
             filtered_docs.append(d)
         else:
             print("---GRADE: DOCUMENT NOT RELEVANT")
-            web_search = True
             continue
 
-    return {"documents": documents, "question": question, "web_search": web_search}
+    if not filtered_docs:
+        transform = True
+
+    return {
+        "documents": filtered_docs,
+        "question": question,
+        "transform": transform,
+        "transform_count": transform_count,
+    }
