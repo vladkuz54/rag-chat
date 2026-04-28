7594121f32b1b64d99b13fbf29b3e79be4c1f22f
vladkuz54 <vkuz1540@gmail.com>
2026-04-28 21:50:12 +0300

    Update main.py to change the input question for app.invoke to a Ukrainian support query

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ load_dotenv()
 from graph.graph import app
 
 if __name__ == "__main__":
-    print(app.invoke(input={"question": "What is the definition of agentic memory"}))
+    print(app.invoke(input={"question": "Я втратив доступ до аккаунту через підозрілий вхід. Мені треба подзвонити за українським номером підтримки 0-800-AI-HELP, щоб розблокувати його?"}))
