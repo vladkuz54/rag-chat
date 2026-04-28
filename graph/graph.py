c6ca7d52ccdd1b194669f91586bf5af9a39266c1
vladkuz54 <vkuz1540@gmail.com>
2026-04-28 20:27:20 +0300

    Add function to retrieve OpenAI API key from AWS Secrets Manager

--- graph/graph.py
+++ graph/graph.py
@@ -12,6 +12,33 @@ from graph.nodes import (generate, generate_generic_response, grade_documents,
                          retrieve, transform)
 from graph.state import GraphState
 
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "openai_key"
+    region_name = "eu-north-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    os.environ.get["OPENAI_API_KEY"] = secret
+
+get_secret()
+
 MAX_TRANSFORMS = 2
 
 
--- main.py
+++ main.py
@@ -2,6 +2,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "openai_key"
+    region_name = "eu-north-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    os.environ.get["OPENAI_API_KEY"] = secret
+
+get_secret()
+
 from graph.graph import app
 
 if __name__ == "__main__":
